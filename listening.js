dc81b53b378c70e8502eb2ae8dc87cf96b0dc015
Victoria Lee <vlee@berkeley.edu>
2023-08-26 14:22:11 -0700

    button tracking

---
+++ listening.js
@@ -0,0 +1,4 @@
+console.log("test")
+const listener = document.getElementById("listener")
+listener.addEventListener("keypress", (event) => console.log(event.key))
+/* document.addEventListener("keypress", (event) => console.log(event.key)) */
