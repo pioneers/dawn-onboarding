ee835a606fcaf4a656eaf70e78403ff598397f2f
Victoria Lee <vlee@berkeley.edu>
2023-08-26 14:32:31 -0700

    logs button presses basic

--- listening.js
+++ listening.js
@@ -1,4 +1,9 @@
 console.log("test")
 const listener = document.getElementById("listener")
-listener.addEventListener("keypress", (event) => console.log(event.key))
+listener.addEventListener("keypress", (e) => {
+    if (e.key == "w" || e.key == "a" || e.key == "s" || e.key == "d") {
+        event.preventDefault();
+        document.getElementById(e.key).click();
+        console.log(e.key)
+      }})
 /* document.addEventListener("keypress", (event) => console.log(event.key)) */
