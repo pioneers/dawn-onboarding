8d34e5072869dd74cd7be9ee2536228bce58849f
Brandon <bwong928@berkeley.edu>
2023-08-26 14:59:29 -0700

    buttons

--- listening.js
+++ listening.js
@@ -1,4 +1,15 @@
-console.log("test")
-const listener = document.getElementById("listener")
-listener.addEventListener("keypress", (event) => console.log(event.key))
+console.log("test");
+const listener = document.getElementById("listener");
+listener.addEventListener("keypress", (event) => console.log(event.key));
 /* document.addEventListener("keypress", (event) => console.log(event.key)) */
+
+const keys = document.querySelectorAll(".key");
+
+keys.forEach((key) => {
+  console.log("testing", key);
+  key.addEventListener("click", () => {
+    console.log("HELLOWORLD", key.id);
+
+    
+  })
+});
