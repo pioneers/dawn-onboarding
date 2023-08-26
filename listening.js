565852cab8fc5fc442aaa91f12255129dd88f6cf
Victoria Lee <vlee@berkeley.edu>
2023-08-26 14:41:45 -0700

    not working

--- listening.js
+++ listening.js
@@ -3,7 +3,12 @@ const listener = document.getElementById("listener")
 listener.addEventListener("keypress", (e) => {
     if (e.key == "w" || e.key == "a" || e.key == "s" || e.key == "d") {
         event.preventDefault();
-        document.getElementById(e.key).click();
-        console.log(e.key)
+        const id = e.key
+        document.getElementById('w').click();
+        clicked(id)
       }})
 /* document.addEventListener("keypress", (event) => console.log(event.key)) */
+
+function clicked(id) {
+    console.log(id)
+}
