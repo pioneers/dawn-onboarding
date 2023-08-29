39234d980f235812b1d3ec54a2fdc44944c8c8f3
Victoria Lee <vlee@berkeley.edu>
2023-08-28 17:44:36 -0700

    button pressing works

--- listening.js
+++ listening.js
@@ -1,15 +1,26 @@
-console.log("test");
 const listener = document.getElementById("listener");
-listener.addEventListener("keypress", (event) => console.log(event.key));
-/* document.addEventListener("keypress", (event) => console.log(event.key)) */
+
+listener.addEventListener("keydown", (event) => press(event.key));
 
 const keys = document.querySelectorAll(".key");
 
 keys.forEach((key) => {
   console.log("testing", key);
-  key.addEventListener("click", () => {
-    console.log("HELLOWORLD", key.id);
-
-    
+  key.addEventListener("mousedown", () => {
+    press(key.id);    
   })
 });
+
+function press(id) {
+  const key = document.getElementById(id)
+  var bgColor = window.getComputedStyle(key).backgroundColor;
+  key.style.backgroundColor = "gray";
+  listener.addEventListener("keyup", function(event) {
+    if (event.key === id) { 
+      key.style.backgroundColor = bgColor;
+    }
+  })
+  listener.addEventListener("mouseup", function(event) {
+    key.style.backgroundColor = bgColor;
+  })
+}
