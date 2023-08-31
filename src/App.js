a7c5e5a43dd1a650e6cbef0f604f2106f4ba43e5
brandogn <bwong928@berkeley.edu>
2023-08-30 21:24:10 -0700

    boilerplate

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Keypad from "./Components/Keypad.js";
+
+function App() {
+  return <div className="App">Hello World!
+    <Keypad />
+  </div>;
+}
+
+export default App;
