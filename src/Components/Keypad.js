fe6aac97e1b2be9413be3e74b586f4ee5e3cdece
Victoria Lee <vlee@berkeley.edu>
2023-09-02 19:10:47 -0700

    compatable with keyboard

--- src/Components/Keypad.js
+++ src/Components/Keypad.js
@@ -2,24 +2,6 @@ import React, { useEffect, useState } from "react";
 import './style.css';
 import { createElement } from 'react';
 
-function State({msg}) {
-  const text = {
-    "W": "0",
-    "A": "0",
-    "S": "0",
-    "D": "0"
-  }
-  const state = createElement(
-    'div',
-    {
-      className: "board",
-      id: 'state'
-    }, 
-    JSON.stringify(text)
-  )
-  return state
-}
-
 function Keys(props) {
   const key = createElement(
     'div',
@@ -43,12 +25,75 @@ export default function Keypad() {
   const [isAPressed, setIsAPressed] = useState(false);
   const [isSPressed, setIsSPressed] = useState(false);
   const [isDPressed, setIsDPressed] = useState(false);
+
+  useEffect(() => {
+    const listener = document.getElementById("listener");
+    listener.addEventListener("keydown", (event) => press(event.key));
+    listener.addEventListener("keyup", (event) => unpress(event.key));
+
+    function press(id) {
+      if (id === "w") {
+        setIsWPressed(true)
+      }
+      if (id === "a") {
+        setIsAPressed(true)
+      }
+      if (id === "s") {
+        setIsSPressed(true)
+      }
+      if (id === "d") {
+        setIsDPressed(true)
+      }
+    }
+
+    function unpress(id) {
+      if (id === "w") {
+        setIsWPressed(false)
+      }
+      if (id === "a") {
+        setIsAPressed(false)
+      }
+      if (id === "s") {
+        setIsSPressed(false)
+      }
+      if (id === "d") {
+        setIsDPressed(false)
+      }
+    }
+  });
+
+  useEffect(() => {
+    state();
+  }, [isWPressed, isAPressed, isSPressed, isDPressed]);
+
+  function state() {
+    const dict = {
+      "W": "0",
+      "A": "0",
+      "S": "0",
+      "D": "0"
+    }
+    if (isWPressed) {
+      dict["W"] = "1";
+    }
+    if (isAPressed) {
+      dict["A"] = "1";
+    }
+    if (isSPressed) {
+      dict["S"] = "1";
+    }
+    if (isDPressed) {
+      dict["D"] = "1";
+    }
+    console.log(dict);
+  }
+
   return <div id="listener">
   <button>Keyboard mode</button>
   <div>{isWPressed.toString()}, {isAPressed.toString()}, {isSPressed.toString()}, {isDPressed.toString()}</div>
   <div className="keypad">
     <Empty/>
-    <Keys name='W' onMouseDown={(e) => {e.stopPropagation(); console.log('w'); setIsWPressed(true);}} onMouseUp={() => setIsWPressed(false)}/>
+    <Keys name='W' onMouseDown={() => setIsWPressed(true)} onMouseUp={() => setIsWPressed(false)}/>
     <Empty/>
     <Keys name='A' onMouseDown={() => setIsAPressed(true)} onMouseUp={() => setIsAPressed(false)}/>
     <Keys name='S' onMouseDown={() => setIsSPressed(true)} onMouseUp={() => setIsSPressed(false)}/>
