4d6c2f5bac4837497146c830f0662a4bcab7eb34
Victoria Lee <vlee@berkeley.edu>
2023-09-02 20:48:28 -0700

    color changes

--- src/Components/Keypad.js
+++ src/Components/Keypad.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import './style.css';
 import { createElement } from 'react';
 
+// Component for key(WASD). Takes in props of name, onMouseDown, onMouseup. name to give an 
+// id and label and onMousDown/Up to call function to change state. 
 function Keys(props) {
   const key = createElement(
     'div',
@@ -16,56 +18,91 @@ function Keys(props) {
   return key
 }
 
+// Component for an empty key.
 function Empty() {
   return <div className="key empty"></div>
 }
 
+// The whole keypad which renders in App.js
 export default function Keypad() {
+  // These const useStates are used to keep track of what key is being pressed. Default is false = not pressed.
   const [isWPressed, setIsWPressed] = useState(false);
   const [isAPressed, setIsAPressed] = useState(false);
   const [isSPressed, setIsSPressed] = useState(false);
   const [isDPressed, setIsDPressed] = useState(false);
 
+  // Change the specific state based on specific id of key pressed.
+  function press(id) {
+    if (id === "w") {
+      setIsWPressed(true)
+    }
+    if (id === "a") {
+      setIsAPressed(true)
+    }
+    if (id === "s") {
+      setIsSPressed(true)
+    }
+    if (id === "d") {
+      setIsDPressed(true)
+    }
+  }
+
+  // We need to track both presses and unpresses. 
+  function unpress(id) {
+    if (id === "w") {
+      setIsWPressed(false)
+    }
+    if (id === "a") {
+      setIsAPressed(false)
+    }
+    if (id === "s") {
+      setIsSPressed(false)
+    }
+    if (id === "d") {
+      setIsDPressed(false)
+    }
+  }
+
+  // useEffect occurs after the return(rendering). This part tracks whether keys on the keyboard are pressed and triggers the 
+  // press and unpress function.
   useEffect(() => {
     const listener = document.getElementById("listener");
     listener.addEventListener("keydown", (event) => press(event.key));
     listener.addEventListener("keyup", (event) => unpress(event.key));
-
-    function press(id) {
-      if (id === "w") {
-        setIsWPressed(true)
-      }
-      if (id === "a") {
-        setIsAPressed(true)
-      }
-      if (id === "s") {
-        setIsSPressed(true)
-      }
-      if (id === "d") {
-        setIsDPressed(true)
-      }
-    }
-
-    function unpress(id) {
-      if (id === "w") {
-        setIsWPressed(false)
-      }
-      if (id === "a") {
-        setIsAPressed(false)
-      }
-      if (id === "s") {
-        setIsSPressed(false)
-      }
-      if (id === "d") {
-        setIsDPressed(false)
-      }
-    }
   });
 
+  // useEffect occurs after the return. This useEffect tracks change in any of the useStates and calls the state() function
+  // to create and send the state to console(later change to send it where we need).
   useEffect(() => {
     state();
+    const keyW = document.getElementById('W')
+    const keyA = document.getElementById('A')
+    const keyS = document.getElementById('S')
+    const keyD = document.getElementById('D')
+
+    if (isWPressed) {
+      keyW.style.backgroundColor = 'grey'
+    } else {
+      keyW.style.backgroundColor = '#e74c3c'
+    }
+    if (isAPressed) {
+      keyA.style.backgroundColor = 'grey'
+    } else {
+      keyA.style.backgroundColor = '#2ecc71'
+    }
+    if (isSPressed) {
+      keyS.style.backgroundColor = 'grey'
+    } else {
+      keyS.style.backgroundColor = '#e67e22'
+    }
+    if (isDPressed) {
+      keyD.style.backgroundColor = 'grey'
+    } else {
+      keyD.style.backgroundColor = '#9b59b6'
+    }
   }, [isWPressed, isAPressed, isSPressed, isDPressed]);
 
+  // Simple dictionary
   function state() {
     const dict = {
       "W": "0",
@@ -93,11 +130,11 @@ export default function Keypad() {
   <div>{isWPressed.toString()}, {isAPressed.toString()}, {isSPressed.toString()}, {isDPressed.toString()}</div>
   <div className="keypad">
     <Empty/>
-    <Keys name='W' onMouseDown={() => setIsWPressed(true)} onMouseUp={() => setIsWPressed(false)}/>
+    <Keys name='W' id='W' onMouseDown={() => setIsWPressed(true)} onMouseUp={() => setIsWPressed(false)}/>
     <Empty/>
-    <Keys name='A' onMouseDown={() => setIsAPressed(true)} onMouseUp={() => setIsAPressed(false)}/>
-    <Keys name='S' onMouseDown={() => setIsSPressed(true)} onMouseUp={() => setIsSPressed(false)}/>
-    <Keys name='D' onMouseDown={() => setIsDPressed(true)} onMouseUp={() => setIsDPressed(false)}/>
+    <Keys name='A' id='A' onMouseDown={() => setIsAPressed(true)} onMouseUp={() => setIsAPressed(false)}/>
+    <Keys name='S' id='S' onMouseDown={() => setIsSPressed(true)} onMouseUp={() => setIsSPressed(false)}/>
+    <Keys name='D' id='D' onMouseDown={() => setIsDPressed(true)} onMouseUp={() => setIsDPressed(false)}/>
   </div>
 </div>;
 }
