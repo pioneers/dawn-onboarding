f8e3a3034e5da5034ae6f7a9c22b17165ea5dc01
Victoria Lee <vlee@berkeley.edu>
2023-10-28 17:52:35 -0700

    connecttion?

--- src/Components/Keypad.js
+++ src/Components/Keypad.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import './style.css';
 import { createElement } from 'react';
 
@@ -25,11 +25,34 @@ function Empty() {
 
 // The whole keypad which renders in App.js
 export default function Keypad() {
+  //foreign code:
+  function useInterval(callback, delay) {
+    const savedCallback = useRef();
+
+    // Remember the latest callback.
+    useEffect(() => {
+      savedCallback.current = callback;
+    }, [callback]);
+
+    // Set up the interval.
+    useEffect(() => {
+      function tick() {
+        savedCallback.current();
+      }
+      if (delay !== null) {
+        let id = setInterval(tick, delay);
+        return () => clearInterval(id);
+      }
+    }, [delay]);
+  }
+
   // These const useStates are used to keep track of what key is being pressed. Default is false = not pressed.
   const [isWPressed, setIsWPressed] = useState(false);
   const [isAPressed, setIsAPressed] = useState(false);
   const [isSPressed, setIsSPressed] = useState(false);
   const [isDPressed, setIsDPressed] = useState(false);
+  // State for connection
+  const [isConnected, setIsConnected] = useState(false);
 
   // Change the specific state based on specific id of key pressed.
   function press(id) {
@@ -63,10 +86,21 @@ export default function Keypad() {
     }
   }
 
+  // useEffect occurs after the return(rendering). This part tracks whether keys on the keyboard are pressed and triggers the 
+  // press and unpress function.
+  useEffect(() => {
+    const listener = document.getElementById("listener");
+    listener.addEventListener("keydown", (event) => {
+      press(event.key)
+    });
+    listener.addEventListener("keyup", (event) => {
+      unpress(event.key)
+    });
+  });
+
   // useEffect occurs after the return. This useEffect tracks change in any of the useStates and calls the state() function
   // to create and send the state to console(later change to send it where we need).
   useEffect(() => {
-    state();
     const keyW = document.getElementById('W')
     const keyA = document.getElementById('A')
     const keyS = document.getElementById('S')
@@ -94,32 +128,6 @@ export default function Keypad() {
     }
   }, [isWPressed, isAPressed, isSPressed, isDPressed]);
 
-  // Send data
-  const sendData = (curr_state) => {
-    fetch('http://localhost:5000/', {
-      'method' : 'POST',
-      headers: { "Content-Type" : "application/json" },
-      body: JSON.stringify({"curr_state" : curr_state})
-    }).then(resp => resp.json())
-    .catch(error => console.log(error));
-  }
-
-  // useEffect occurs after the return(rendering). This part tracks whether keys on the keyboard are pressed and triggers the 
-  // press and unpress function.
-  useEffect(() => {
-    const listener = document.getElementById("listener");
-    listener.addEventListener("keydown", (event) => {
-      press(event.key)
-      const curr_state = JSON.stringify(state())
-      sendData(curr_state)
-    });
-    listener.addEventListener("keyup", (event) => {
-      unpress(event.key)
-      const curr_state = JSON.stringify(state())
-      sendData(curr_state)
-    });
-  });
-
   // Simple dictionary
   function state() {
     const dict = {
@@ -128,6 +136,9 @@ export default function Keypad() {
       "S": "0",
       "D": "0"
     }
+
+    console.log(isWPressed, isAPressed, isSPressed, isDPressed)
+
     if (isWPressed) {
       dict["W"] = "1";
     }
@@ -140,20 +151,45 @@ export default function Keypad() {
     if (isDPressed) {
       dict["D"] = "1";
     }
-    console.log(dict);
     return dict;
   }
 
+  // Send data
+  const sendData = (curr_state) => {
+    console.log('sending', curr_state)
+    fetch('http://localhost:5000/', {
+      'method' : 'POST',
+      headers: { "Content-Type" : "application/json" },
+      body: JSON.stringify({"curr_state" : curr_state})
+    }).then(resp => resp.json())
+    .catch(error => console.log(error));
+  }
+  
+  // Creates sending loop
+  useInterval((put = state())  => {
+    sendData(put)}, isConnected? 3000 : null);
+  
+  const reconnect = () => {
+    setIsConnected(true);
+  }
+  const disconnect = () => {
+    setIsConnected(false);
+  }
+  
   return <div id="listener">
   <button>Keyboard mode</button>
   <div>{isWPressed.toString()}, {isAPressed.toString()}, {isSPressed.toString()}, {isDPressed.toString()}</div>
+  <div>
+    <button onClick={reconnect}>Connect</button>
+    <button onClick={disconnect}>Disconnect</button>
+  </div>
   <div className="keypad">
     <Empty/>
-    <Keys name='W' id='W' onMouseDown={() => setIsWPressed(true)} onMouseUp={() => setIsWPressed(false)}/>
+    <Keys name='W' id='W' onMouseDown={() => press('w')} onMouseUp={() => unpress('w')}/>
     <Empty/>
-    <Keys name='A' id='A' onMouseDown={() => setIsAPressed(true)} onMouseUp={() => setIsAPressed(false)}/>
-    <Keys name='S' id='S' onMouseDown={() => setIsSPressed(true)} onMouseUp={() => setIsSPressed(false)}/>
-    <Keys name='D' id='D' onMouseDown={() => setIsDPressed(true)} onMouseUp={() => setIsDPressed(false)}/>
+    <Keys name='A' id='A' onMouseDown={() => press('a')} onMouseUp={() => unpress('a')}/>
+    <Keys name='S' id='S' onMouseDown={() => press('s')} onMouseUp={() => unpress('s')}/>
+    <Keys name='D' id='D' onMouseDown={() => press('d')} onMouseUp={() => unpress('d')}/>
   </div>
 </div>;
 }
