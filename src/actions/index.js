c1563084ab8217743491a448f753f4dffc24a9e7
brandogn <bwong928@berkeley.edu>
2023-11-01 20:13:35 -0700

    updated refactored code

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,13 +1,3 @@
-// export const sendData = (curr_state, ip) => {
-//   console.log("sending", curr_state);
-//   fetch(ip, {
-//     method: "POST",
-//     headers: { "Content-Type": "application/json" },
-//     body: JSON.stringify({ curr_state: curr_state }),
-//   })
-//     .then((resp) => resp.json())
-//     .catch((error) => console.log(error));
-// };
 
 export const dataLayer = {
   sendData: function(curr_state, ip) {
@@ -19,7 +9,9 @@ export const dataLayer = {
     })
       .then((resp) => resp.json())
       .catch((error) => console.log(error));
+  },
+
+  ping: function() {
+
   }
-
-
 }
--- src/components/Keypad2.js
+++ src/components/Keypad2.js
@@ -1,14 +1,17 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { dataLayer } from "../actions/index.js";
+import "./style.css";
 
 const UP = "UP",
   DOWN = "DOWN",
   LEFT = "LEFT",
-  RIGHT = "RIGHT";
+  RIGHT = "RIGHT",
+  EMPTY = "EMPTY";
 
 const ROBOT_IP = "http://localhost:5000/"; // (fake robot)
 
-const KeyHandler = () => {
+function KeyHandler() {
+  const [connected, setConnected] = useState(false);
   const [keyCount, setKeyCount] = useState({
     UP: 0,
     LEFT: 0,
@@ -71,23 +74,72 @@ const KeyHandler = () => {
     }));
   };
 
-  const directions = [UP, LEFT, RIGHT, DOWN];
+  function Key(props) {
+    const dir = props.dir;
+    return (
+      <div
+        className="key"
+        onMouseDown={() => handleButtonClick(dir, true)}
+        onMouseUp={() => handleButtonClick(dir, false)}
+      >
+        {dir.charAt(0).toUpperCase() + dir.slice(1)}
+      </div>
+    );
+  }
+
+  /* Ping interval */
+  function useInterval(callback, delay) {
+    const savedCallback = useRef();
+
+    // Remember the latest callback.
+    useEffect(() => {
+      savedCallback.current = callback;
+    }, [callback]);
+
+    // Set up the interval.
+    useEffect(() => {
+      function tick() {
+        savedCallback.current();
+      }
+      if (delay !== null) {
+        let id = setInterval(tick, delay);
+        return () => clearInterval(id);
+      }
+    }, [delay]);
+  }
+
+  // Creates sending loop. Delay is null, so does not loop when connected is false. Delay is 3s when connected is true.
+  useInterval(
+    () => {
+      dataLayer.sendData(keyCount, ROBOT_IP);
+    },
+    connected ? 500 : null
+  );
+
+  const reconnect = () => {
+    setConnected(true);
+  };
+  const disconnect = () => {
+    setConnected(false);
+  };
 
   return (
     <div>
-      <div>Press WASD or Arrow keys (try not to use the UI buttons)</div>
-      {directions.map((dir) => (
-        <button
-          onMouseDown={() => handleButtonClick(dir, true)}
-          onMouseUp={() => handleButtonClick(dir, false)}
-        >
-          {dir.charAt(0).toUpperCase() + dir.slice(1)}
-        </button>
-      ))}
+      <div>Press WASD or Arrow keys (UI buttons are buggy)</div>
+      <button onClick={reconnect}>Connect</button>
+      <button onClick={disconnect}>Disconnect</button>
+      <div className="keypad">
+        <div className="key empty" />
+        <Key dir={UP} />
+        <div className="key empty" />
+        <Key dir={LEFT} />
+        <Key dir={DOWN} />
+        <Key dir={RIGHT} />
+      </div>
 
       <pre>{JSON.stringify(keyCount, null, 2)}</pre>
     </div>
   );
-};
+}
 
 export default KeyHandler;
