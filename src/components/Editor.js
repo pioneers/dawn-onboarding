01b4539a6b2aef23ef6c709710a3ea6a870823ce
Victoria Lee <vlee@berkeley.edu>
2024-03-20 23:45:50 -0700

    toolbar

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -2,9 +2,11 @@ import { dataLayer } from "../actions/index.js";
 import React from "react";
 import { render } from "react-dom";
 import AceEditor from "react-ace";
+import {Card, ButtonGroup, DropdownButton, Dropdown, Form} from 'react-bootstrap';
 import "ace-builds/src-noconflict/mode-python";
 import "ace-builds/src-noconflict/theme-github";
 import "ace-builds/src-noconflict/ext-language_tools";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Editor() {
 
@@ -14,16 +16,34 @@ function Editor() {
 
   // Render editor
   return (
-    <div>    
-      <AceEditor
-      mode="python"
-      theme="github"
-      width="100%"
-      onChange={onChange}
-      name="CodeEditor"
-      editorProps={{ $blockScrolling: true }}
-      />
-  </div>
+    <Card>    
+      <Card.Header>
+        <Card.Title style={{ fontSize: '14px' }}>
+          Editor
+        </Card.Title>
+      </Card.Header>
+      <Card.Body>
+        <Form inline>
+          <ButtonGroup id="file-operations-buttons">
+            <DropdownButton title="File">
+              <Dropdown.Item >New File</Dropdown.Item>
+              <Dropdown.Item >Open</Dropdown.Item>
+              <Dropdown.Item >Save</Dropdown.Item>
+              <Dropdown.Item >Save As</Dropdown.Item>
+            </DropdownButton>
+          </ButtonGroup>
+        </Form>
+        
+        <AceEditor
+        mode="python"
+        theme="github"
+        width="100%"
+        onChange={onChange}
+        name="CodeEditor"
+        editorProps={{ $blockScrolling: true }}
+        />
+      </Card.Body>
+  </Card>
   );
 }
 
